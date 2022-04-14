a1a9e3f8f530e1c5ece1d3f1ca76fdc6cbd050ce
Andrew Petersen <fespetersen1990@gmail.com>
2022-04-14 10:20:55 -0600

    Finished FizzBuzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,1 +1,15 @@
 // Write your solution below this line:
+
+let num1 = 1;
+
+for (let i = num1; i <= 50; i++) {
+    if ( i % 3 === 0 && i % 5 === 0 ) {
+        console.log("Fizzbuzz");
+    } else if ( i % 3 === 0) {
+        console.log("Fizz");
+    } else if ( i % 5 === 0) {
+        console.log("Buzz")
+    } else if ( i % 3 !== 0 && i % 5 !== 0) {
+        console.log(i)
+    }
+}
